1dce1c4339b24a8785ee1b3bddded738abb88613
Jonathan NAIM <jonathan.naim@outlook.com>
2016-08-05 17:07:12 +0100

    Add Webpack config that will allow us to package our tool

---
+++ AdaptableBlotter/AdaptableBlotter/webpack.config.js
@@ -0,0 +1,37 @@
+﻿//var webpack = require('webpack');
+//module.exports = {
+//    entry: [
+//      "./App_Scripts/MyReactComponent.js"
+//    ],
+//    output: {
+//        path: __dirname + '/dist/',
+//        filename: "bundle-webpack.js"
+//    },
+//    module: {
+//    },
+//    plugins: [
+//      new webpack.NoErrorsPlugin()
+//    ]
+
+//};
+
+
+module.exports = {
+    entry: [
+      "./App_Scripts/MyReactComponent.tsx"
+    ],
+    output: {
+        path: __dirname + '/dist/',
+        filename: "bundle-webpack.js"
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+          // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+          { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }
+}
