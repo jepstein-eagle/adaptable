42d2e2bda2fbee23b0245a733035e540db5ae33e
Jonathan NAIM <jonathan.naim@outlook.com>
2016-08-30 08:50:07 +0100

    fixing typings

--- AdaptableBlotter/typings/index.d.ts
+++ AdaptableBlotter/typings/index.d.ts
@@ -4,6 +4,6 @@
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
+/// <reference path="globals/redux-storage/index.d.ts" />
 /// <reference path="globals/redux/index.d.ts" />
 /// <reference path="modules/redux-storage-engine-localstorage/index.d.ts" />
-/// <reference path="modules/redux-storage/index.d.ts" />
