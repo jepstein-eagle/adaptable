8abd831faea73618437b226240feb0e3ae0b894b
Jonathan NAIM <jonathan.naim@outlook.com>
2016-08-31 11:18:19 +0100

    breaking the build on purpose

--- AdaptableBlotter/App_Scripts/Redux/ActionsReducers/MenuRedux.ts
+++ AdaptableBlotter/App_Scripts/Redux/ActionsReducers/MenuRedux.ts
@@ -29,7 +29,7 @@ export const MenuItemClicked = (MenuItem: IMenuItem): MenuItemClickedAction => (
     MenuItem
 })
 
-
+breaking the build on purpose
 const initialMenuState: MenuState = {
     MenuItems: []
 }
