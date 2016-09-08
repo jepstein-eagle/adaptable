c4f9f25800523f3d63cab8322a5639f112745ad4
Jonathan NAIM <jonathan.naim@outlook.com>
2016-09-08 16:14:44 +0100

    Adding an helper class with random shit

---
+++ AdaptableBlotter/App_Scripts/Core/Helper.ts
@@ -0,0 +1,11 @@
+export module Helper {
+    export function getChar(event: KeyboardEvent) {
+        if (event.which == null) {
+            return String.fromCharCode(event.keyCode) // IE
+        } else if (event.which != 0 && event.charCode != 0) {
+            return String.fromCharCode(event.which)   // the rest
+        } else {
+            return null // special key
+        }
+    }
+}
