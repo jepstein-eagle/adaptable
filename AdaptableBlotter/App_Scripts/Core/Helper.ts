629629b8dfe6f21cd42072a8942327106d6346ea
Jonathan NAIM <jonathan.naim@outlook.com>
2016-09-08 16:30:52 +0100

    Overload of the Helper to handle difference events types. Typescript is quite smart sometimes

--- AdaptableBlotter/App_Scripts/Core/Helper.ts
+++ AdaptableBlotter/App_Scripts/Core/Helper.ts
@@ -1,5 +1,7 @@
 export module Helper {
-    export function getChar(event: KeyboardEvent) {
+    export function getChar(event: JQueryEventObject):string;
+    export function getChar(event: KeyboardEvent):string;
+    export function getChar(event: JQueryKeyEventObject | KeyboardEvent):string{
         if (event.which == null) {
             return String.fromCharCode(event.keyCode) // IE
         } else if (event.which != 0 && event.charCode != 0) {
