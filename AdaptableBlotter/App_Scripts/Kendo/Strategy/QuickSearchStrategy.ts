17d94c28a7ae919d924984c35edb56ccac2abcf2
jonny.wolfson@gmail.com <TradersTools@TradersToolss-iMac.local>
2016-11-17 09:45:29 +0200

    rerunning quick search if properties change

--- AdaptableBlotter/App_Scripts/Kendo/Strategy/QuickSearchStrategy.ts
+++ AdaptableBlotter/App_Scripts/Kendo/Strategy/QuickSearchStrategy.ts
@@ -4,11 +4,12 @@ import { AdaptableStrategyBase } from '../../Core/AdaptableStrategyBase';
 import * as StrategyIds from '../../Core/StrategyIds'
 import { IMenuItem } from '../../Core/Interface/IStrategy';
 import { IAdaptableBlotter } from '../../Core/Interface/IAdaptableBlotter';
-import { MenuType } from '../../Core/Enums';
+import { MenuType, SearchStringOperator } from '../../Core/Enums';
 
 
 export class QuickSearchStrategy extends AdaptableStrategyBase implements IQuickSearchStrategy {
     private quickSearchText: string
+    private searchStringOperator: SearchStringOperator
 
     private menuItemConfig: IMenuItem;
     constructor(blotter: IAdaptableBlotter) {
@@ -19,9 +20,17 @@ export class QuickSearchStrategy extends AdaptableStrategyBase implements IQuick
     }
 
     InitState() {
+       // Run search if the quick search has changed
         let stateQuickSearchText: string = this.blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchText;
         if (this.quickSearchText !=null && this.quickSearchText != stateQuickSearchText) {
             this.quickSearchText = stateQuickSearchText;
+            this.blotter.SearchService.ApplyQuickSearch();
+        }
+        
+        // Run search if the operator has changed and search is not empty
+        let stateQuickSearchStringOperator: SearchStringOperator = this.blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.SearchStringOperator;
+             if (this.searchStringOperator !=null && this.searchStringOperator != stateQuickSearchStringOperator && stateQuickSearchText!="") {
+            this.searchStringOperator = stateQuickSearchStringOperator;
             this.blotter.SearchService.ApplyQuickSearch();
         }
     }
