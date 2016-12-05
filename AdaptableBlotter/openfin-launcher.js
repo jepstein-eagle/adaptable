b8144569d5e0904d4a68593afc0d178962aea382
Jonathan NAIM <jonathan.naim@outlook.com>
2016-12-05 13:32:51 +0000

    Open fin config.

---
+++ AdaptableBlotter/openfin-launcher.js
@@ -0,0 +1,11 @@
+﻿var openfinLauncher = require('openfin-launcher')
+
+openfinLauncher.launchOpenFin({
+        configPath: 'http://localhost:8080/app.json'
+    })
+    .then(function() {
+        console.log('success!')
+    })
+    .fail(function(error) {
+        console.log('error!', error)
+    })
