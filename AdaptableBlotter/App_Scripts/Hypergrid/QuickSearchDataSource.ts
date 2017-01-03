3e4b4aac1fb20c42b958ee4cb09e76c3baa22d61
Jonathan NAIM <jonathan.naim@outlook.com>
2017-01-03 18:35:31 +0000

    Use StringExtensions.IsNotNullOrEmpty in QuickSearch

--- AdaptableBlotter/App_Scripts/Hypergrid/QuickSearchDataSource.ts
+++ AdaptableBlotter/App_Scripts/Hypergrid/QuickSearchDataSource.ts
@@ -1,15 +1,16 @@
 import { IAdaptableBlotter } from '../Core/Interface/IAdaptableBlotter'
 import { DataSourceIndexed } from './DataSourceIndexed'
+import { StringExtensions } from '../Core/Extensions'
 
 //All custom pipelines should extend from pipelineBase
 export let QuickSearchDataSource = (blotter: IAdaptableBlotter) => DataSourceIndexed.extend('QuickSearchDataSource', {
     blotter: blotter,
     apply: function () {
-        if (blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchText != "") {
-            this.buildIndex(this.filterTest);
-        } else {
-            this.clearIndex();
-        }
+        if (StringExtensions.IsNotNullOrEmpty(blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchText)){
+                this.buildIndex(this.filterTest);
+            } else {
+                this.clearIndex();
+            }
     },
     filterTest: function (r: any, rowObject: any) {
         //Need to use the operator and case sensitive
