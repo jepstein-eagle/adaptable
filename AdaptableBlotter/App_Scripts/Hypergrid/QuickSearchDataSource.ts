c491cf820f8238e02fe02725dd79b8bf3b0ef6c1
Jonathan NAIM <jonathan.naim@outlook.com>
2017-01-06 14:57:32 +0000

    Handle QuickSearch Operator in Hypergrid

--- AdaptableBlotter/App_Scripts/Hypergrid/QuickSearchDataSource.ts
+++ AdaptableBlotter/App_Scripts/Hypergrid/QuickSearchDataSource.ts
@@ -1,24 +1,45 @@
 import { IAdaptableBlotter } from '../Core/Interface/IAdaptableBlotter'
 import { DataSourceIndexed } from './DataSourceIndexed'
 import { StringExtensions } from '../Core/Extensions'
+import { LeafExpressionOperator } from '../Core/Enums'
 
 //All custom pipelines should extend from pipelineBase
 export let QuickSearchDataSource = (blotter: IAdaptableBlotter) => DataSourceIndexed.extend('QuickSearchDataSource', {
     blotter: blotter,
     apply: function () {
-        if (StringExtensions.IsNotNullOrEmpty(blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchText)){
-                this.buildIndex(this.filterTest);
-            } else {
-                this.clearIndex();
-            }
+        if (StringExtensions.IsNotNullOrEmpty(blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchText)) {
+            this.buildIndex(this.filterTest);
+        } else {
+            this.clearIndex();
+        }
     },
     filterTest: function (r: any, rowObject: any) {
         //Need to use the operator and case sensitive
         for (let prop in rowObject) {
             let stringValueLowerCase = String(rowObject[prop]).toLowerCase()
             let quickSearchLowerCase = blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchText.toLowerCase()
-            if (stringValueLowerCase.includes(quickSearchLowerCase)) {
-                return true
+            switch (blotter.AdaptableBlotterStore.TheStore.getState().QuickSearch.QuickSearchOperator) {
+                case LeafExpressionOperator.Contains:
+                    {
+                        if (stringValueLowerCase.includes(quickSearchLowerCase)) {
+                            return true;
+                        }
+                    }
+                    break;
+                case LeafExpressionOperator.StartsWith:
+                    {
+                        if (stringValueLowerCase.startsWith(quickSearchLowerCase)) {
+                            return true;
+                        }
+                    }
+                    break;
+                case LeafExpressionOperator.EndsWith:
+                    {
+                        if (stringValueLowerCase.endsWith(quickSearchLowerCase)) {
+                            return true;
+                        }
+                    }
+                    break;
             }
         }
         return false;
