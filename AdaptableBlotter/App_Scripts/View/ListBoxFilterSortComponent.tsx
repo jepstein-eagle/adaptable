0eee5e1378746103e38d39e0d2794694a66849b6
Jonathan NAIM <jonathan.naim@outlook.com>
2017-02-01 18:03:09 +0000

    SingleListBox: Make the Search Bit at top go all way across irrespective of width

--- AdaptableBlotter/App_Scripts/View/ListBoxFilterSortComponent.tsx
+++ AdaptableBlotter/App_Scripts/View/ListBoxFilterSortComponent.tsx
@@ -17,30 +17,28 @@ interface ListBoxFilterSortComponentProps extends React.ClassAttributes<ListBoxF
 export class ListBoxFilterSortComponent extends React.Component<ListBoxFilterSortComponentProps, {}> {
     render() {
         return <Form horizontal>
-            <Row style={{ display: "flex", alignItems: "center" }}>
-                <Col xs={10}>
-                    <FormGroup style={{ margin: 0 }}>
-                        <InputGroup>
-                            <FormControl
-                                type="text"
-                                value={this.props.FilterValue}
-                                placeholder="Search"
-                                onChange={(e) => this.handleChangeFilterValue(e)}
-                                />
-                            <InputGroup.Button>
-                                <Button onClick={() => this.clearFilter()}><Glyphicon glyph="remove" /></Button>
-                            </InputGroup.Button>
-                        </InputGroup>
-                    </FormGroup>
+            <FormGroup style={{ margin: 0 }}>
+                <InputGroup>
+                    <FormControl
+                        type="text"
+                        value={this.props.FilterValue}
+                        placeholder="Search"
+                        onChange={(e) => this.handleChangeFilterValue(e)}
+                        />
+                    <InputGroup.Button>
+                        <Button onClick={() => this.clearFilter()}><Glyphicon glyph="remove" /></Button>
+                    </InputGroup.Button>
+                    <InputGroup.Button>
+                        {this.props.SortOrder == SortOrder.Ascending ?
+                            <Button onClick={() => this.props.sortColumnValues()} ><Glyphicon glyph="sort-by-alphabet" /></Button> :
+                            <Button onClick={() => this.props.sortColumnValues()} ><Glyphicon glyph="sort-by-alphabet-alt" /></Button>
+                        }
+                    </InputGroup.Button>
+                </InputGroup>
+                <InputGroup>
 
-                </Col>
-                <Col xs={2}>
-                    {this.props.SortOrder == SortOrder.Ascending ?
-                        <Button bsSize="small" onClick={() => this.props.sortColumnValues()} style={{ float: "right" }}><Glyphicon className="scale-lg" glyph="sort-by-alphabet" /></Button> :
-                        <Button bsSize="small" onClick={() => this.props.sortColumnValues()} style={{ float: "right" }}><Glyphicon className="scale-lg" glyph="sort-by-alphabet-alt" /></Button>
-                    }
-                </Col>
-            </Row>
+                </InputGroup>
+            </FormGroup>
         </Form>
     }
 
