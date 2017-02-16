7b2f2feeeeeb1504d573633bc86ccf0b51cc5f8f
Jonathan NAIM <jonathan.naim@outlook.com>
2017-02-16 10:47:24 +0000

    just keeping the cmds for Demos

---
+++ AdaptableBlotter/Demo/demoCleanElasticSearch.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\Jonathan\Downloads\curl_X64_ssl\winssl
+pause
+curl -XDELETE "http://localhost:9200/*
---
+++ AdaptableBlotter/Demo/demoLogStash.cmd
@@ -0,0 +1,2 @@
+cd C:\Users\Jonathan\Downloads\logstash-5.1.2\bin
+logstash -f logstash.config
