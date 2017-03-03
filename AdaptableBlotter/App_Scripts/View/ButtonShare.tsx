1f580b127ff77fd7889d6f285f5b9975cddfc6c4
Jonathan NAIM <jonathan.naim@outlook.com>
2017-03-03 19:35:47 +0000

    Create ButtonShare

---
+++ AdaptableBlotter/App_Scripts/View/ButtonShare.tsx
@@ -0,0 +1,24 @@
+/// <reference path="../../typings/index.d.ts" />
+
+import * as React from "react";
+import * as Redux from "redux";
+import { Helper } from '../Core/Helper'
+import { Button, OverlayTrigger, Tooltip, Glyphicon } from 'react-bootstrap';
+import { IUIError } from '../Core/Interface/IStrategy';
+import { IConfigEntity } from '../Core/Interface/IAdaptableBlotter';
+import * as PopupRedux from '../Redux/ActionsReducers/PopupRedux'
+import { ButtonBase, ButtonProps } from './ButtonBase'
+
+export class ButtonShare extends React.Component<ButtonProps, {}> {
+    render() {
+        return <ButtonBase ToolTip="Edit"
+            bsStyle='info'
+            ConfigEntity={this.props.ConfigEntity}
+            glyph="share"
+            onClick={() => this.props.onClick()}
+            overrideDisableButton={this.props.overrideDisableButton}
+            overrideTooltip={this.props.overrideTooltip}
+            style={this.props.style}
+        />;
+    }
+}
