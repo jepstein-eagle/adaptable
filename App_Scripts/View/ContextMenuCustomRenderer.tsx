f721cda13646fb3d27354721cf8e9de791b95d63
Jonathan NAIM <jo.naim@adaptabletools.com>
2017-05-15 15:37:48 +0100

    Fix context menu

--- App_Scripts/View/ContextMenuCustomRenderer.tsx
+++ App_Scripts/View/ContextMenuCustomRenderer.tsx
@@ -6,10 +6,10 @@ export class ContextMenuCustomRenderer extends React.Component<any, {}> {
         var className = this.props.className + " " + this.props.classPrefix + 'menu-item-button',
             data = this.props.data,
             chevron = this.props.isExpandable ? <span className='fa fa-chevron-right fa-fw'></span> : null;
-
+        let glyph = <Glyphicon glyph={this.props.data.menuitem.GlyphIcon} style={{ 'marginRight': '5px' }} />
         return (
-            <button {...data.handlers} key={data.key} id={data.id} className={className}>
-                <Glyphicon glyph={this.props.data.menuitem.GlyphIcon} style={{ 'marginRight': '5px' }} />
+            <button {...data.handlers} key={data.key}  id={data.id} className={className}>
+                {glyph}
                 {data.menuitem.Label}
                 {chevron}
             </button>
