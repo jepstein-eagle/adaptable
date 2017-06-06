3a9e26a742831e3532c7c137cebb6aeb966a90c0
Jonathan NAIM <jo.naim@adaptabletools.com>
2017-06-06 17:29:22 +0100

    remove the themes from the package

--- webpack.dev.js
+++ webpack.dev.js
@@ -33,7 +33,7 @@ module.exports = {
             'process.env.NODE_ENV': JSON.stringify('development')
         }),
         //this makes sure we package it in the dist folder and make it available for the webpack dev server
-        new CopyWebpackPlugin([{ context: 'themes', from: '**/*', to: 'adaptable-blotter-themes' }]),
+        // new CopyWebpackPlugin([{ context: 'themes', from: '**/*', to: 'adaptable-blotter-themes' }]),
         // new CopyWebpackPlugin([{ from: 'stylesheets/adaptableblotter-style.css', to: '' }]),
         new CopyWebpackPlugin([{ from: 'ExtLibs/**/*', to: '' }])
     ],
