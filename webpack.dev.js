6e796e4b4e2353e910edf99c9e6511b0088d9d18
Jonathan NAIM <jo.naim@adaptabletools.com>
2017-07-07 11:32:42 +0100

    speeding up slightly the build in dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,15 +3,41 @@ var path = require('path');
 var failPlugin = require('webpack-fail-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    entry: {
+var conditionalEntry;
+if (process.env.arg == "all") {
+    conditionalEntry = {
         'adaptableblotterkendo': ["./App_Scripts/Vendors/Kendo/AdaptableBlotter.ts"],
-        'adaptableblotterdx': ["./App_Scripts/Vendors/DevExpress/AdaptableBlotter.ts"],
         'adaptableblotterhypergrid': ["./App_Scripts/Vendors/Hypergrid/AdaptableBlotter.ts"],
         'adaptableblottergrid': ["./App_Scripts/Vendors/AdaptableGrid/AdaptableBlotter.ts"],
         'adaptableblotteraggrid': ["./App_Scripts/Vendors/agGrid/AdaptableBlotter.ts"],
         harness: "./Harness/DataGenerator.ts"
-    },
+    };
+}else if (process.env.arg == "kendo") {
+        conditionalEntry = {
+        'adaptableblotterkendo': ["./App_Scripts/Vendors/Kendo/AdaptableBlotter.ts"],
+        harness: "./Harness/DataGenerator.ts"
+    };
+}else if (process.env.arg == "hypergrid") {
+        conditionalEntry = {
+        'adaptableblotterhypergrid': ["./App_Scripts/Vendors/Hypergrid/AdaptableBlotter.ts"],
+        harness: "./Harness/DataGenerator.ts"
+    };
+}else if (process.env.arg == "adaptablegrid") {
+        conditionalEntry = {
+        'adaptableblottergrid': ["./App_Scripts/Vendors/AdaptableGrid/AdaptableBlotter.ts"],
+        harness: "./Harness/DataGenerator.ts"
+    };
+}
+else if (process.env.arg == "aggrid") {
+        conditionalEntry = {
+        'adaptableblotteraggrid': ["./App_Scripts/Vendors/agGrid/AdaptableBlotter.ts"],
+        harness: "./Harness/DataGenerator.ts"
+    };
+}
+
+
+module.exports = {
+    entry: conditionalEntry,
     output: {
         path: __dirname + '/dist',
         filename: "[name]-bundle.js",
@@ -45,9 +71,9 @@ module.exports = {
                 test: /\.ts(x?)$/, loader: 'babel-loader?presets[]=es2015&plugins[]=transform-runtime!ts-loader'
             },
             // handle main stylesheets required 
-            { test: /\.css$/,  exclude: /themes/, loader: 'style-loader!css-loader' },
-                        // handle main stylesheets required 
-            { test: /\.css$/,  exclude: /stylesheets/, loader: 'css-to-string-loader!css-loader' },
+            { test: /\.css$/, exclude: /themes/, loader: 'style-loader!css-loader' },
+            // handle main stylesheets required 
+            { test: /\.css$/, exclude: /stylesheets/, loader: 'css-to-string-loader!css-loader' },
             { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
     },
