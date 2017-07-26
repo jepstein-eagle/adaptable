4c8cab44677a3159f40ae3454cc16d46b58b4734
Jonathan NAIM <jo.naim@adaptabletools.com>
2017-07-26 10:38:12 +0100

    fix bug in CustomSortagGridStrategy when both elements are not in the list but equal. We return now 0 (useful for when both elements are undefined due to grouping so we don't constantly reorder the grouping parent rows) #112

--- App_Scripts/Strategy/CustomSortagGridStrategy.ts
+++ App_Scripts/Strategy/CustomSortagGridStrategy.ts
@@ -19,6 +19,9 @@ export class CustomSortagGridStrategy extends CustomSortStrategy {
             let containsSecondElement = indexSecondElement >= 0;
             //if none of the element are in the list we jsut return normal compare
             if (!containsFirstElement && !containsSecondElement) {
+                if (firstElementValue == secondElementValue) {
+                    return 0
+                }
                 return (firstElementValue < secondElementValue) ? -1 : 1;
             }
             //if first item not in the list make sure we put it after the second item
