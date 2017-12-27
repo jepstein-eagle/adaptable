59bafc8760460a8c98818468c337f82da08a3f91
Jonathan NAIM <jo.naim@adaptabletools.com>
2017-12-27 14:47:13 +0000

    Revert "webpack version update to latest and update of webpack script accordingly. #156"
    This reverts commit 0e72890b5acf82f22e1313f3e94f6d9be2cb450c.

--- webpack.dev.js
+++ webpack.dev.js
@@ -43,15 +43,16 @@ module.exports = {
         library: "[name]",
         libraryTarget: 'umd'
     },
+    // Turn on sourcemaps
+    devtool: 'source-map',
+
     resolve: {
-        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
     },
 
     plugins: [
         failPlugin,
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[file].map'
-        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('development')
         }),
