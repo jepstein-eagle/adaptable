0dcca6ba68dcfaee19d517bc84e6c55f32953efd
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2018-03-20 15:23:06 +0200

    updating ag grid harness for local demos

--- Harness/agGrid/indexharnessaggrid.js
+++ Harness/agGrid/indexharnessaggrid.js
@@ -112,34 +112,39 @@ function replacer(a, b, c) {
 }
 
 function getSchema(data) {
-    var schema = [],
-        firstRow = Array.isArray(data) && data[0];
+    var schema = []
 
-    firstRow = (typeof firstRow === 'object') ? firstRow : {};
-    for (var p in firstRow) {
-        if (firstRow.hasOwnProperty(p)) {
-            if (p === 'ask' || p === 'bid' || p === 'bloombergAsk' || p === 'bloombergBid') {
-                schema.push({ headerName: capitalize(p), field: p });
-            }
-            else if (p === 'isLive') {
-                schema.push({ headerName: capitalize(p), field: p, filter: 'text' });
-            }
-            // grouped columns
-            else if (p === 'counterparty' || p === 'country' || p === 'currency' || p === 'lastUpdatedBy') {
-                schema.push({ headerName: capitalize(p), field: p, filter: 'text', enableRowGroup: true });
-            }
-            else if (p === 'price') {
-                schema.push({ headerName: capitalize(p), field: p, filter: 'text' });
-            }
-            else if (p === 'notional') {  // has own render and is groupable
-                schema.push({ headerName: capitalize(p), field: p, editable: true, filter: 'text', cellRenderer: notionalCellRenderer, enableRowGroup: true });
-            } else if (p.includes("Date")) {
-                schema.push({ headerName: capitalize(p), field: p, editable: true, cellEditorParams: { useFormatter: true }, valueParser: dateParseragGrid, valueFormatter: shortDateFormatteragGrid });
-            } else {
-                schema.push({ headerName: capitalize(p), field: p, editable: true, filter: 'text' });
-            }
-        }
-    }
+    schema.push({ headerName: "Trade Id", field: "tradeId", editable: true, filter: 'text' });
+    schema.push({ headerName: "notional", field: "notional", editable: true, filter: 'text', cellRenderer: notionalCellRenderer, enableRowGroup: true });
+    schema.push({ headerName: "DeskId", field: "deskId", filter: 'text' });
+    schema.push({ headerName: "Counterparty", field: "counterparty", filter: 'text', enableRowGroup: true });
+    schema.push({ headerName: "Country", field: "country", filter: 'text', enableRowGroup: true });
+    schema.push({ headerName: "Currency", field: "currency", filter: 'text', enableRowGroup: true });
+    schema.push({ headerName: "Change On Year", field: "changeOnYear", filter: 'text' });
+
+    schema.push({
+        headerName: "Pricing",
+        children: [
+            { headerName: "Bid Offer Spread", field: "bidOfferSpread", columnGroupShow: 'open', editable: true, cellClass: 'number-cell'},
+            { headerName: "Price", field: "price", columnGroupShow: 'open', editable: true, cellClass: 'number-cell'},
+            { headerName: "Ask", field: "ask", columnGroupShow: 'closed', cellClass: 'number-cell'},
+            { headerName: "Bid", field: "bid", columnGroupShow: 'closed', cellClass: 'number-cell'},
+            { headerName: "Bloomberg Ask", field: "bloombergAsk", columnGroupShow: 'closed', cellClass: 'number-cell'},
+            { headerName: "Bloomberg Bid", field: "bloombergBid", columnGroupShow: 'closed', cellClass: 'number-cell'}
+        ]
+    })
+    schema.push({ headerName: "Is Live", field: "isLive",editable: false, cellRenderer: params => {
+        return `<input type='checkbox' ${params.value ? 'checked' : ''} />`;
+    }});
+    schema.push({ headerName: "Fitch Rating", field: "fitchRating", editable: true, filter: 'text' });
+    schema.push({ headerName: "Moodys Rating", field: "moodysRating", editable: true, filter: 'text' });
+    schema.push({ headerName: "SandP Rating", field: "sandpRating", editable: true, filter: 'text' });
+    schema.push({ headerName: "Trade Date", field: "tradeDate", editable: true, cellEditorParams: { useFormatter: true }, valueParser: dateParseragGrid, valueFormatter: shortDateFormatteragGrid });
+    schema.push({ headerName: "Settlement Date", field: "settlementDate", editable: true, cellEditorParams: { useFormatter: true }, valueParser: dateParseragGrid, valueFormatter: shortDateFormatteragGrid });
+    schema.push({ headerName: "Percent Change", field: "percentChange", filter: 'text' });
+    schema.push({ headerName: "Last Updated By", field: "lastUpdatedBy", filter: 'text', enableRowGroup: true });
+    schema.push({ headerName: "Last Updated", field: "lastUpdated", editable: true, cellEditorParams: { useFormatter: true }, valueParser: dateParseragGrid, valueFormatter: shortDateFormatteragGrid });
+
     return schema;
 }
 var currencyFormatter = new Intl.NumberFormat('en-US', {
@@ -166,7 +171,7 @@ function InitBlotter() {
         enableRangeSelection: true,
         animateRows: true,
         enableFilter: true,
-
+        enableColResize: true
     };
 
 
