a27a69c39aec2372c4e93064d972d75ac606331a
Samed Duzcay <samedduzcay@gmail.com>
2018-08-28 19:56:06 +0300

    Add missing modules

--- packages/adaptableblotter/index.js
+++ packages/adaptableblotter/index.js
@@ -1,8 +1,8 @@
 var adaptableBlotterHypergrid = require('./dist/adaptableblotterhypergrid-bundle.min');
 var adaptableBlotterKendo = require('./dist/adaptableblotterkendo-bundle.min');
 var adaptableBlotteragGrid = require('./dist/adaptableblotteraggrid-bundle.min');
-var BlotterFactory = require('./dist/App_Scripts/Core/BlotterFactory).BlotterFactory;
-var AdaptableBlotterApp = require('./dist/App_Scripts/View/AdaptableBlotterView).AdaptableBlotterApp;
+var BlotterFactory = require('./dist/App_Scripts/Core/BlotterFactory').BlotterFactory;
+var AdaptableBlotterApp = require('./dist/App_Scripts/View/AdaptableBlotterView').AdaptableBlotterApp;
 var IAdaptableBlotter = require('./dist/App_Scripts/Core/Interface/IAdaptableBlotter').IAdaptableBlotter;
 var IAdaptableBlotterOptions = require('./dist/App_Scripts/Core/Api/Interface/IAdaptableBlotterOptions').IAdaptableBlotterOptions;
 
