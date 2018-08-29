a848f21ae1d5833d196e19a05cd69306cc429aea
Samed Duzcay <samedduzcay@gmail.com>
2018-08-29 18:47:16 +0300

    Provide themes in the adaptableblotter factory

--- packages/adaptableblotter-react/dist/lib/index.js
+++ packages/adaptableblotter-react/dist/lib/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+require("ag-grid-enterprise");
 var AdaptableBlotter_1 = require("./AdaptableBlotter");
 exports.AdaptableBlotter = AdaptableBlotter_1.default;
 var AdaptableBlotterAgGrid_1 = require("./AdaptableBlotterAgGrid");
--- packages/adaptableblotter/factory.js
+++ packages/adaptableblotter/factory.js
@@ -1,5 +1,9 @@
 var BlotterFactory = require('./dist/App_Scripts/Core/BlotterFactory').BlotterFactory;
 var AdaptableBlotterApp = require('./dist/App_Scripts/View/AdaptableBlotterView').AdaptableBlotterApp;
+var AdaptableBlotterThemes = require('./dist/App_Scripts/Styles/themes');
+
+// todo: use themes in the clients
 
 module.exports.BlotterFactory = BlotterFactory;
 module.exports.AdaptableBlotterApp = AdaptableBlotterApp;
+module.exports.AdaptableBlotterThemes = AdaptableBlotterThemes;
