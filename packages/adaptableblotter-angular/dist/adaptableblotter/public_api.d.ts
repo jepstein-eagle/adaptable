eff2dc24de044724140aa5e30323b9d2b89393cb
Samed Duzcay <samedduzcay@gmail.com>
2018-09-01 15:57:54 +0300

    Remove /dist from git

--- packages/adaptableblotter-angular/dist/adaptableblotter/public_api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/adaptableblotter.module';
-export * from './lib/adaptableblotter.component';
-export * from './lib/adaptableblotter-aggrid.component';
-export * from 'adaptableblotter/types';
