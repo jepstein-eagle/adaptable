aac5a3f24e4a2bdeae09e32e7495337f6252351e
Samed Duzcay <samedduzcay@gmail.com>
2018-09-13 02:17:06 +0300

    Create template for hypergrid angular wrapper

---
+++ packages/adaptableblotter-angular/projects/adaptableblotter/src/lib/adaptableblotter-hypergrid.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'adaptable-blotter-hypergrid',
+  template: `<div>Wow</div>`,
+})
+export class AdaptableblotterHyperGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
