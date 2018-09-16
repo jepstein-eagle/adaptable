61befb34603e2c955adbb8eeb12d5b780b44bf07
Samed Duzcay <samedduzcay@gmail.com>
2018-09-17 00:15:26 +0300

    Assign random IDs to blotter containers

--- packages/adaptableblotter-angular/projects/adaptableblotter/src/lib/adaptableblotter.component.ts
+++ packages/adaptableblotter-angular/projects/adaptableblotter/src/lib/adaptableblotter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, Output, EventEmitter } from '@angular/core';
 
 import * as ReactDOM from 'adaptableblotter/node_modules/react-dom';
 import { BlotterFactory, AdaptableBlotterApp } from 'adaptableblotter/factory';
@@ -6,25 +6,30 @@ import { IAdaptableBlotter, IAdaptableBlotterOptions } from 'adaptableblotter/ty
 
 @Component({
   selector: 'adaptable-blotter',
-  template: `<div id="adaptableBlotter"><div>Loading...</div></div>`,
+  template: `<div [id]="adaptableBlotterOptions.adaptableBlotterContainer">Loading...</div>`,
   styles: []
 })
 export class AdaptableBlotterComponent implements OnInit {
   @Input() adaptableBlotterOptions: IAdaptableBlotterOptions;
   @Input() vendorGridName: 'agGrid' | 'Hypergrid' | 'Kendo' | 'AdaptableGrid';
 
+  @Output() adaptableBlotterMounted = new EventEmitter<any>();
+
   private adaptableBlotter: IAdaptableBlotter;
 
   constructor(private elRef: ElementRef) {}
 
   ngOnInit() {
+    this.adaptableBlotterOptions.adaptableBlotterContainer =
+      this.adaptableBlotterOptions.adaptableBlotterContainer || `adaptableBlotter-${Math.random() * 10000 | 0}`;
     this.adaptableBlotter = BlotterFactory.CreateAdaptableBlotter(
       this.adaptableBlotterOptions,
       this.vendorGridName
     );
+    this.adaptableBlotterMounted.emit(this.adaptableBlotter);
     ReactDOM.render(
       AdaptableBlotterApp({ AdaptableBlotter: this.adaptableBlotter }),
-      this.elRef.nativeElement.firstChild.firstChild,
+      this.elRef.nativeElement.firstChild,
     );
   }
 
