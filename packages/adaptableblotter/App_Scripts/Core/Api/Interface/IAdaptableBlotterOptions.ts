f75e6d795ca9295ff7c39242cebd7c8962f813a8
Adaptable Tools <adaptabletools@Adaptables-MacBook-Pro.local>
2018-10-09 10:53:44 +0100

    changed notes

--- packages/adaptableblotter/App_Scripts/Core/Api/Interface/IAdaptableBlotterOptions.ts
+++ packages/adaptableblotter/App_Scripts/Core/Api/Interface/IAdaptableBlotterOptions.ts
@@ -40,20 +40,20 @@ export interface IAdaptableBlotterOptions {
      */
     predefinedConfig?: any;
     /**
-     * Remote config server that'll persist the application state and give it back on demand.
+     * Remote config server that'll persist the user state and give it back on demand.
      * Only used if enableRemoteConfigServer is true.
      *
      * AdaptableBlotter will send a POST request to this URL to persist the state with the follower parameters:
      * Headers: { ab_username: string, ab_id: string }
-     * Body: Stringified application state
+     * Body: Stringified user state
      *
      * Each ab_username & ab_id combination is unique, so your server should persist the state and use that combination as a key.
      *
      * AdaptableBlotter will send a GET request to this URL to get the persisted state with the follower parameters:
      * Headers: { ab_username: string, ab_id: string }
      *
-     * Your server should return the application state related to the given ab_username and ab_id combination as a JSON object.
-     */
+     * Your server should return the user state related to the given ab_username and ab_id combination as a JSON object.
+     */ 
     remoteConfigServerUrl?: string;
     /**
      * How many items to dispay in column value listboxes when building queries
