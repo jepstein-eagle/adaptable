433cf77f59e0bf6a943700fcd0a8d18827dd0c5e
Adaptable Tools <adaptabletools@Adaptables-MBP.lan>
2018-11-26 14:23:16 +0000

    removing adaptablegrid until we do it properly

--- packages/adaptableblotter/dist/App_Scripts/Vendors/AdaptableGrid/BlotterFactoryAdaptableGrid.d.ts
+++
@@ -1,5 +0,0 @@
-import { AdaptableBlotter } from "./AdaptableBlotter";
-import { IAdaptableBlotterOptions } from "../../Core/Api/Interface/IAdaptableBlotterOptions";
-export declare module BlotterFactoryAdaptableGrid {
-    function CreateAdaptableBlotter(blotterOptions: IAdaptableBlotterOptions, renderGrid: boolean): AdaptableBlotter;
-}
