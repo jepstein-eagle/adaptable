f8ce5d0fa147ca8ef40ce1137d5432829a2ce88e
Adaptable Tools <adaptabletools@Adaptables-MacBook-Pro.local>
2018-12-02 13:44:07 +0200

    now version 2.7

--- packages/adaptableblotter/App_Scripts/View/Components/Popups/AdaptableBlotterAbout.tsx
+++ packages/adaptableblotter/App_Scripts/View/Components/Popups/AdaptableBlotterAbout.tsx
@@ -72,7 +72,7 @@ export class AdaptableBlotterAbout extends React.Component<AdaptableBlotterAbout
             let options: IAdaptableBlotterOptions = this.props.AdaptableBlotter.BlotterOptions;
 
             returnRows.push(this.createColItem(colItems, "Vendor Grid", this.props.AdaptableBlotter.VendorGridName));
-            returnRows.push(this.createColItem(colItems, "Adaptable Blotter Version", "2.6"));
+            returnRows.push(this.createColItem(colItems, "Adaptable Blotter Version", "2.7"));
 
             if (options.blotterId != undefined) {
                 returnRows.push(this.createColItem(colItems, "Blotter Id", options.blotterId));
