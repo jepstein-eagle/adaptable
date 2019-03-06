bfacd1cc922b37ae3bb5e71d937c550646b7b491
Adaptable Tools <adaptabletools@MacBook-Pro.lan>
2019-03-06 07:52:19 +0000

    making chartoptions nullable

--- packages/adaptableblotter/App_Scripts/Utilities/Interface/BlotterOptions/IChartOptions.ts
+++ packages/adaptableblotter/App_Scripts/Utilities/Interface/BlotterOptions/IChartOptions.ts
@@ -1,15 +1,15 @@
 export interface IChartOptions {
-    /**
-     * Whether a chart is displayed at start up.
-     * Only applies if there is the CurrentChartName property in Chart State is not empty
-     * Defaults to false
-     */
-    displayOnStartUp: boolean;
-    /**
-     * Whether to show charts in a modal popup
-     * If true, then we ignore the chartContainer property when working out where to show the chart
-     * Defaults to false
-     */
-   showModal: boolean;
+  /**
+   * Whether a chart is displayed at start up.
+   * Only applies if there is the CurrentChartName property in Chart State is not empty
+   * Defaults to false
+   */
+  displayOnStartUp?: boolean;
+  /**
+   * Whether to show charts in a modal popup
+   * If true, then we ignore the chartContainer property when working out where to show the chart
+   * Defaults to false
+   */
+  showModal?: boolean;
 }
 
