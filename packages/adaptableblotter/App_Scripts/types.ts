41cea3f5875800269428a5ba3088ea77b58fa7bc
roblotter <roblotter1@gmail.com>
2019-05-15 15:55:02 +0300

    Organize main package type exports under types

--- packages/adaptableblotter/App_Scripts/types.ts
+++ packages/adaptableblotter/App_Scripts/types.ts
@@ -1,4 +1,22 @@
+import { IEventApi } from './Api/Interface/IEventApi';
+import {
+  ISearchChangedEventArgs,
+  IThemeChangedEventArgs,
+  IStateChangedEventArgs,
+  IColumnStateChangedEventArgs,
+  IAlertFiredEventArgs,
+} from './Utilities/Interface/IStateEvents';
+
 export { IAdaptableBlotter } from './Utilities/Interface/IAdaptableBlotter';
 export {
   IAdaptableBlotterOptions,
 } from './Utilities/Interface/BlotterOptions/IAdaptableBlotterOptions';
+
+export {
+  IEventApi,
+  ISearchChangedEventArgs,
+  IThemeChangedEventArgs,
+  IStateChangedEventArgs,
+  IColumnStateChangedEventArgs,
+  IAlertFiredEventArgs,
+};
