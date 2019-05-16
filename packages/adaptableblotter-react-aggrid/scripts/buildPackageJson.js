0e8ee2e914d43078f73cd3dc491f266d8bb6158f
roblotter <roblotter1@gmail.com>
2019-05-16 12:49:11 +0300

    Fix react wrapper deps

--- packages/adaptableblotter-react-aggrid/scripts/buildPackageJson.js
+++ packages/adaptableblotter-react-aggrid/scripts/buildPackageJson.js
@@ -3,13 +3,19 @@ const chalk = require('chalk');
 const resolve = require('path').resolve;
 
 const sourcePackagePath = resolve(process.cwd(), './package.json');
+const abBlotterPackagePath = resolve(process.cwd(), '../adaptableblotter/package.json');
 
 const packageJSON = require(sourcePackagePath);
+const abPackageJSON = require(abBlotterPackagePath);
 
 function buildGlobalPackageJSON() {
   return new Promise((res, reject) => {
     const toDelete = ['devDependencies', 'scripts', 'private'];
     toDelete.forEach(key => delete packageJSON[key]);
+
+    packageJSON.dependencies = packageJSON.dependencies || {};
+
+    Object.assign(packageJSON.dependencies, abPackageJSON.dependencies || {});
     const content = JSON.stringify(packageJSON, null, 2);
     const path = resolve(process.cwd(), './dist', 'package.json');
     fs.writeFile(path, content, 'utf8', err => {
