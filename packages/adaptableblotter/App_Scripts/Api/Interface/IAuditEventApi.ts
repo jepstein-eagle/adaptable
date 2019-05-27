b736a0d8b1c99163b2fa946fd31b682061de28e4
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-05-27 09:28:31 +0300

    updating audit event text

--- packages/adaptableblotter/App_Scripts/Api/Interface/IAuditEventApi.ts
+++ packages/adaptableblotter/App_Scripts/Api/Interface/IAuditEventApi.ts
@@ -8,11 +8,6 @@ export interface IAuditEventApi {
   _onAuditCellEdited: EventDispatcher<IAdaptableBlotter, IAuditLogEventArgs>;
   _onAuditFunctionApplied: EventDispatcher<IAdaptableBlotter, IAuditLogEventArgs>;
 
-  /**
-   * Event fired whenever the state in the Blotter changes
-   * Provides full coverage of what triggered the change and the new function state.
-   * @returns IEvent<IAdaptableBlotter, IStateChangedEventArgs>
-   */
   onAuditStateChanged(): IEvent<IAdaptableBlotter, IAuditLogEventArgs>;
   onAuditCellEdited(): IEvent<IAdaptableBlotter, IAuditLogEventArgs>;
   onAuditFunctionApplied(): IEvent<IAdaptableBlotter, IAuditLogEventArgs>;
