22c355cb7d29c37f96bd9c780eb3f0c2bbf45be6
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-05-29 14:15:00 +0300

    api changes

--- packages/adaptableblotter/App_Scripts/Api/Interface/IQuickSearchApi.ts
+++ packages/adaptableblotter/App_Scripts/Api/Interface/IQuickSearchApi.ts
@@ -1,27 +1,56 @@
 import { IStyle } from '../../Utilities/Interface/IStyle';
 import { QuickSearchState } from '../../Redux/ActionsReducers/Interface/IState';
-import { DisplayAction } from '../../Utilities/Enums';
 export interface IQuickSearchApi {
-  getQuickSearchState(): QuickSearchState;
-  setQuickSearchState(quickSearchState: QuickSearchState): void;
   /**
-   * Runs QuickSearch on the supplied text
+   * Retrieves all the Quick Search State
+   */
+  getQuickSearchState(): QuickSearchState;
+
+  /**
+   * Replaces (not merges) current Quick Search State with inputted one
+   * @param quickSearchState
+   */
+  setQuickSearchState(quickSearchState: QuickSearchState): void;
+
+  /**
+   * Runs a QuickSearch using the supplied text
    * @param quickSearchText text to run QuickSearch on
    */
   applyQuickSearch(quickSearchText: string): void;
+
   /**
    * Clears Quick Search
    */
   clearQuickSearch(): void;
+
   /**
-   * Retrieves the current quick search text
+   * Retrieves the current Quick Search text
    */
   getQuickSearchValue(): string;
+
+  /**
+   *  Retrieves the current Quick Search style
+   */
   getQuickSearchStyle(): IStyle;
+
+  /**
+   *  Retrieves the current Quick Search display action
+   */
   getQuickSearchDisplayAction(): 'HighlightCell' | 'ShowRow' | 'ShowRowAndHighlightCell';
 
+  /**
+   * Sets the display action for Quick Search
+   * Will effect whether or not rows with no matching cells are displayed
+   * @param displayAction the display action to use
+   */
   setQuickSearchDisplayAction(
     displayAction: 'HighlightCell' | 'ShowRow' | 'ShowRowAndHighlightCell'
   ): void;
+
+  /**
+   * Sets the style for Quick Search
+   * This style can be the name of a css style (which you must provide)
+   * @param style the style to use
+   */
   setQuickSearchStyle(style: IStyle): void;
 }
