a260a5fd886aae8f3b453674eadd5557184661ee
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-06-14 09:01:19 +0100

    updating to use examples helper

--- packages/adaptableblotter-react-aggrid/examples/pages/basicdemo/index-demo.tsx
+++ packages/adaptableblotter-react-aggrid/examples/pages/basicdemo/index-demo.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-
 import { LicenseManager } from 'ag-grid-enterprise';
-import { GridOptions } from 'ag-grid-community';
-
 import AdaptableBlotterReact from '../../../src';
 import '../../../src/base.scss';
 import '../../../src/themes/light.scss';
 import '../../../src/themes/dark.scss';
-
-import { DataGenerator } from '../../../../adaptableblotter/Harness/DataGenerator';
-import { IAdaptableBlotterOptions } from '../../../../adaptableblotter/App_Scripts/types';
-
+import { ExamplesHelper } from '../../ExamplesHelper';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
-
 LicenseManager.setLicenseKey(process.env.AG_GRID_LICENSE!);
-
-const dataGen = new DataGenerator();
-const gridOptions: GridOptions = dataGen.getGridOptionsTrade(50);
-
-const adaptableBlotterOptions: IAdaptableBlotterOptions = {
-  primaryKey: 'tradeId',
-  userName: 'demo user',
-  blotterId: 'basic demo',
-  licenceKey: process.env.ENTERPRISE_LICENSE,
-};
+const examplesHelper = new ExamplesHelper();
 
 export default () => (
   <AdaptableBlotterReact
-    style={{ height: '80vh' }}
-    gridOptions={gridOptions}
-    blotterOptions={adaptableBlotterOptions}
+    style={{ height: '100vh' }}
+    gridOptions={{
+      columnDefs: examplesHelper.getTradeSchema(),
+      rowData: examplesHelper.getTrades(500),
+      enableRangeSelection: true,
+      floatingFilter: true,
+      suppressMenuHide: true,
+    }}
+    blotterOptions={{
+      primaryKey: 'tradeId',
+      blotterId: 'BYOP demo',
+      licenceKey: process.env.ENTERPRISE_LICENSE,
+    }}
   />
 );
--- packages/adaptableblotter-react-aggrid/examples/pages/basicdemo/with-render-function-demo.tsx
+++ packages/adaptableblotter-react-aggrid/examples/pages/basicdemo/with-render-function-demo.tsx
@@ -6,16 +6,15 @@ import { GridOptions } from 'ag-grid-community';
 import AdaptableBlotterReact from '../../../src';
 import '../../../src/index.scss';
 
-import { DataGenerator } from '../../../../adaptableblotter/Harness/DataGenerator';
 import { IAdaptableBlotterOptions } from '../../../../adaptableblotter/App_Scripts/types';
-
+import { ExamplesHelper } from '../../ExamplesHelper';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 LicenseManager.setLicenseKey(process.env.AG_GRID_LICENSE!);
+const examplesHelper = new ExamplesHelper();
 
-const dataGen = new DataGenerator();
-const gridOptions: GridOptions = dataGen.getGridOptionsTrade(500);
+const gridOptions: GridOptions = getGridOptionsTrade(200);
 
 const adaptableBlotterOptions: IAdaptableBlotterOptions = {
   vendorGrid: gridOptions,
