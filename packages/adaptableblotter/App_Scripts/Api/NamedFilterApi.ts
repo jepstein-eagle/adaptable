6483c68d279dc653075fc8a80dbef69a83ac3ee3
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-07-19 11:51:39 +0100

    put named filter api back

--- packages/adaptableblotter/App_Scripts/Api/NamedFilterApi.ts
+++ packages/adaptableblotter/App_Scripts/Api/NamedFilterApi.ts
@@ -6,7 +6,7 @@ import { NamedFilter } from '../PredefinedConfig/RunTimeState/NamedFilterState';
 export class NamedFilterApi extends ApiBase implements INamedFilterApi {
   public getAllNamedFilter(): NamedFilter[] {
     const config = this.blotter.blotterOptions.predefinedConfig as PredefinedConfig;
-    return []; // config.NamedFilter.NamedFilters;  // problem with this is they might not be in config ;()
-    // and currently they are not in state and there is no redux
+    return config.NamedFilter != null ? config.NamedFilter.NamedFilters : [];
+    // currently they are not in state and there is no redux because of function issue...
   }
 }
