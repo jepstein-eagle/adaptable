852d5a963a6ef1e4c33dc4cb75659ab38608d18c
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-07-25 18:13:36 +0100

    docs (ignore)

--- packages/adaptableblotter/App_Scripts/PredefinedConfig/DesignTimeState/ActionColumnState.ts
+++ packages/adaptableblotter/App_Scripts/PredefinedConfig/DesignTimeState/ActionColumnState.ts
@@ -4,7 +4,7 @@ import { AdaptableBlotterObject } from '../AdaptableBlotterObject';
 /**
  * The Predefined Configuration for Action Columns
  *
- * An Action Column is one which shows a button that when clicked fires an onActionColumnClicked event (see[Event Api](https://api.adaptableblotter.com/interfaces/_api_interface_ieventapi_.ieventapi.html))
+ * An Action Column is one which shows a button that when clicked fires an onActionColumnClicked event (see [Event Api](https://api.adaptableblotter.com/interfaces/_api_interface_ieventapi_.ieventapi.html))
  *
  * You can choose to render the cell contents yourself (via the *RenderFunctionName* property).
  *
@@ -23,7 +23,7 @@ export interface ActionColumn extends AdaptableBlotterObject {
   /**
    * The id of the Column
    *
-   * If no value is set for *FriendlyName* then this will be the name of the Column also (e.g. what appears in the Column Header)
+   * If no value is set for the *FriendlyName* property, then this will be the name of the Column also (e.g. what appears in the Column Header)
    */
   ColumnId: string;
   /**
@@ -33,7 +33,7 @@ export interface ActionColumn extends AdaptableBlotterObject {
    */
   FriendlyName?: string;
   /**
-   * What text to put on the button.
+   * The text to put on the button.
    *
    * Leave blank if you wish to render the Button separately (using the *RenderFunctionName* property).
    */
