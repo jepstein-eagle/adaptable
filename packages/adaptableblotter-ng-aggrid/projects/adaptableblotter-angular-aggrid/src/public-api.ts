80a93b59c1192e5be004788a2eef5b8568f8abb7
roblotter <roblotter1@gmail.com>
2019-11-18 15:23:26 +0200

    add public-api.ts back into ng project

---
+++ packages/adaptableblotter-ng-aggrid/projects/adaptableblotter-angular-aggrid/src/public-api.ts
@@ -0,0 +1,13 @@
+/*
+ * Public API Surface of adaptableblotter-angular-aggrid
+ */
+
+// export * from './lib/adaptableblotter-angular-aggrid.service';
+
+export { AdaptableBlotterAngularAgGridModule } from './lib/adaptableblotter-angular-aggrid.module';
+
+export { AdaptableBlotterAngularAgGridComponent } from './lib/adaptableblotter-angular-aggrid.component';
+
+/*
+ * Public API Surface of adaptableblotter-angular-aggrid
+ */
