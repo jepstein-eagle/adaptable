a8ad587542c7c245e64ed5e00f876c96aa9b3610
roblotter <roblotter1@gmail.com>
2019-11-19 13:32:16 +0200

    update

--- packages/adaptableblotter-ng-aggrid/projects/adaptableblotter-angular-aggrid/src/public-api.ts
+++ packages/adaptableblotter-ng-aggrid/projects/adaptableblotter-angular-aggrid/src/public-api.ts
@@ -4,9 +4,7 @@
 
 // export * from './lib/adaptableblotter-angular-aggrid.service';
 
-export { AdaptableBlotterAngularAgGridModule } from './adaptableblotter-angular-aggrid.module';
-
-export { AdaptableBlotterAngularAgGridComponent } from './adaptableblotter-angular-aggrid.component';
+export * from './adaptableblotter-angular-aggrid.module';
 
 export * from './adaptableblotter/types';
 /*
