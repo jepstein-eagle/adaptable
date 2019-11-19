f872c12e30b15e0c9c52c11517238fbc1bc7d983
Roblotter <50020244+roblotter@users.noreply.github.com>
2019-11-19 16:11:43 +0200

    Fix angular prod (#591)

--- packages/adaptableblotter-ng-aggrid/projects/adaptableblotter-angular-aggrid/src/public-api.ts
+++ packages/adaptableblotter-ng-aggrid/projects/adaptableblotter-angular-aggrid/src/public-api.ts
@@ -2,17 +2,6 @@
  * Public API Surface of adaptableblotter-angular-aggrid
  */
 
-// export * from './lib/adaptableblotter-angular-aggrid.service';
-import { AdaptableBlotterAngularAgGridComponent } from './lib/adaptableblotter-angular-aggrid.component';
 export * from './lib/adaptableblotter-angular-aggrid.component';
 export * from './lib/adaptableblotter-angular-aggrid.module';
 export * from './adaptableblotter/App_Scripts/types';
-
-export {
-  AdaptableBlotterAngularAgGridComponent,
-} from './lib/adaptableblotter-angular-aggrid.component';
-export default AdaptableBlotterAngularAgGridComponent;
-
-/*
- * Public API Surface of adaptableblotter-angular-aggrid
- */
