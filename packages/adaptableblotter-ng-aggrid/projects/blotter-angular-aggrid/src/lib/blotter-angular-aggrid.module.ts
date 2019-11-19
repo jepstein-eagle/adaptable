6db77a444af16b001afb055d6436f0d9560d0bcc
roblotter <roblotter1@gmail.com>
2019-11-19 15:38:23 +0200

    update

--- packages/adaptableblotter-ng-aggrid/projects/blotter-angular-aggrid/src/lib/blotter-angular-aggrid.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BlotterAngularAggridComponent } from './blotter-angular-aggrid.component';
-
-@NgModule({
-  declarations: [BlotterAngularAggridComponent],
-  imports: [],
-  exports: [BlotterAngularAggridComponent],
-})
-export class BlotterAngularAggridModule {}
--- packages/adaptableblotter-ng-aggrid/projects/blotter-angular-aggrid/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of blotter-angular-aggrid
- */
-
-export * from './lib/blotter-angular-aggrid.service';
-export * from './lib/blotter-angular-aggrid.component';
-export * from './lib/blotter-angular-aggrid.module';
