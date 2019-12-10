24e2e20afd5eb895aeb9be5b20ad28e8a7dae6cf
roblotter <roblotter1@gmail.com>
2019-12-10 10:34:35 +0200

    release version canary

--- scripts/registry-publish.js
+++ scripts/registry-publish.js
@@ -3,7 +3,7 @@
 const { spawnSync } = require('child_process');
 
 if (process.env.PUBLISH_PACKAGE_CMD) {
-  console.log('starting publishing');
+  console.log('starting publishing with ' + process.env.PUBLISH_PACKAGE_CMD);
   spawnSync(process.env.PUBLISH_PACKAGE_CMD, {
     stdio: 'inherit',
     env: { ...process.env },
