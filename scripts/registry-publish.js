dc84fcb49ecbdac54e12f1bfd3931f5839c25ab0
roblotter <roblotter1@gmail.com>
2019-12-10 14:20:37 +0200

    update

--- scripts/registry-publish.js
+++ scripts/registry-publish.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 
-const { spawnSync } = require('child_process');
+const { spawn } = require('child_process');
+
+const exec = (cmd, args = []) =>
+  new Promise((resolve, reject) => {
+    console.log(`Started: ${cmd} ${args.join(' ')}`);
+    const app = spawn(cmd, args, { stdio: 'inherit' });
+    app.on('close', resolve);
+    app.on('error', reject);
+  });
+
+const main = async () => {
+  await exec(process.env.PUBLISH_PACKAGE_CMD);
+};
 
 if (process.env.PUBLISH_PACKAGE_CMD) {
   console.log('starting publishing with ' + process.env.PUBLISH_PACKAGE_CMD);
-  spawnSync(process.env.PUBLISH_PACKAGE_CMD, {
-    stdio: 'inherit',
-    env: { ...process.env },
+
+  main().catch(err => {
+    console.error(err);
+    console.error(err.stack);
+    process.exit(-1);
   });
 } else {
   console.log('Nothing to publish');
