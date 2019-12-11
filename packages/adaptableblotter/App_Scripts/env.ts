9c2f27b7e6f59236afdcee37cada914834b9d0a3
roblotter <roblotter1@gmail.com>
2019-12-11 19:13:19 +0200

    Fix release workflow

--- packages/adaptableblotter/App_Scripts/env.ts
+++ packages/adaptableblotter/App_Scripts/env.ts
@@ -1,7 +1,6 @@
 module.exports = {
   IPUSHPULL_API_KEY: process.env.IPUSHPULL_API_KEY,
   IPUSHPULL_API_SECRET: process.env.IPUSHPULL_API_SECRET,
-  NODE_ENV: process.env.NODE_ENV,
 }; /** don't add TS type annotations here!
 Dont make this a module by putting EXPORT DEFAULT
 For the build process, this has to be plain JS
