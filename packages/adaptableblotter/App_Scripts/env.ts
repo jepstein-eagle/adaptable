2cfede0bc67cddeb949423b15c461f9420a91e5c
roblotter <roblotter1@gmail.com>
2019-12-11 19:23:29 +0200

    Release version canary

--- packages/adaptableblotter/App_Scripts/env.ts
+++ packages/adaptableblotter/App_Scripts/env.ts
@@ -1,7 +1,7 @@
 module.exports = {
   IPUSHPULL_API_KEY: process.env.IPUSHPULL_API_KEY,
   IPUSHPULL_API_SECRET: process.env.IPUSHPULL_API_SECRET,
-}; /** don't add TS type annotations here!
+}; /** Don't add TS type annotations here!
 Dont make this a module by putting EXPORT DEFAULT
 For the build process, this has to be plain JS
 */
