3fd24028f3e99f5040fc3a5e8fd2c0c1ac71b82a
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-01-22 14:15:21 +0200

    sparkline api (ignore)

--- packages/adaptable/src/PredefinedConfig/SparklineColumnState.ts
+++ packages/adaptable/src/PredefinedConfig/SparklineColumnState.ts
@@ -4,10 +4,17 @@ import { AdaptableObject } from './Common/AdaptableObject';
 /**
  * The Predefined Configuration for Sparkline Columns
  *
- * An Sparkline Column is a column whose values are array of numbers, so each cell can be represented as a sparkline chart
+ * A Sparkline Column is a column which contains an array of numbers as its value.
  *
+ * As a result, each cell in the column can be represented as a sparkline chart.
  *
- * ```ts
+ *  **Further AdapTable Help Resources**
+ *
+ * [Demo Site](https://demo.adaptabletools.com/column/aggridsparklinecolumnsdemo/) | [API](_api_sparklinecolumnapi_.sparklinecolumnapi.html) | [FAQ](https://adaptabletools.zendesk.com/hc/en-us/articles/360002449478-Sparkline-Column-FAQ) | [Videos](https://adaptabletools.zendesk.com/hc/en-us/articles/360002449498-Sparkline-Column-Videos) | [User Guide](https://adaptabletools.zendesk.com/hc/en-us/articles/360003213038-Special-Column-Functions)
+ *
+ * **Sparkline Column Predefined Config Example**
+ *
+ * * ```ts
  * export default {
  * SparklineColumn: {
  *  SparklineColumns: [
@@ -19,6 +26,7 @@ import { AdaptableObject } from './Common/AdaptableObject';
  *      MinimumValue: 20,
  *      MaximumValue: 1000,
  *      SparklineType: 'Line'
+ *      ShowToolTip: true
  *   },
  *  ],
  *  },
@@ -27,7 +35,21 @@ import { AdaptableObject } from './Common/AdaptableObject';
  */
 export interface SparklineColumnState extends DesignTimeState {
   /**
-   * The list of column you want to represent with sparklines
+   * The list of columns you want to represent as `SparklineColumn`
+   *
+   * Each SparklineColumn contains the following properties:
+   *
+   * - ColumnId: Name of the Column (will appear in the header)
+   *
+   * - SparklineType: 'Line', 'Column' or 'Area'
+   *
+   * - MinimumValue: The min value to show;
+   *
+   * - MaximumValue: The max value to show;
+   *
+   * - ShowToolTip: Whether to show a tooltip displaying the underlying values;
+   *
+   * - LineColor: The colour to use if the type is 'Line';
    */
   SparklineColumns?: SparklineColumn[];
 }
@@ -57,6 +79,7 @@ export interface SparklineColumn extends AdaptableObject {
    * The value to be considered the maximum value for the sparkline chart of the column
    */
   MaximumValue?: number;
+
   /**
    * Whether to show a tooltip which will display the underlying values in the Sparklne
    */
