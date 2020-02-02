68b1c198f5637a8cf05933dfa974ebaed9f99998
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-02-02 19:03:06 +0200

    api (ignore)

--- packages/adaptable/src/AdaptableOptions/SearchOptions.ts
+++ packages/adaptable/src/AdaptableOptions/SearchOptions.ts
@@ -19,9 +19,9 @@ export interface SearchOptions {
    *
    * Leave unset (default is 'None') to perform everything on the client.
    *
-   * This allows you to perform a mixture of client and server searching e.g. run Column Filters on the client but Advanced Seearch on the server.
+   * This allows you to perform a mixture of client and server searching e.g. run Column Filters on the client but Advanced Search on the server.
    *
-   * If an option is set to be run on the Server then AdapTable will not do any filtering but it will fire the `SearchChanged` event.
+   * If an option is set to be run on the Server then AdapTable will not do any relevant searching or filtering, but it **will** fire the [`SearchChanged` event](_api_events_searchchanged_.searchchangedeventargs.html).
    *
    * This event provides full details of the current search and filter state in AdapTable, together with what triggered the event.
    *
