105f3575e7f08b2ce3a7e1612dab1e5d8c1d927d
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-03-03 23:54:39 +0200

    weird

--- packages/adaptable/typedocconfig.ts
+++ packages/adaptable/typedocconfig.ts
@@ -1,7 +1,7 @@
 module.exports = {
   src: ['./src/'],
   mode: 'modules',
-  includeDeclarations: true,
+  includeDeclarations: false,
   exclude: [
     './src/agGrid/**/*',
     './src/View/**/*',
