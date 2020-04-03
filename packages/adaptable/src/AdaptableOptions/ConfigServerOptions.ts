641b3f5b8cfe7885290062a830c28651edb45b06
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-04-03 11:55:11 +0300

    config docs

--- packages/adaptable/src/AdaptableOptions/ConfigServerOptions.ts
+++ packages/adaptable/src/AdaptableOptions/ConfigServerOptions.ts
@@ -1,9 +1,9 @@
 /**
  * Options for managing Config Server.
  *
- * ConfigServer enables you to save user state **remotely** (the default is to save state in local storage).
+ * ConfigServer enables Adaptable State to be saved **remotely** (the default is in local storage).
  *
- * Note that since v.6 AdapTable provides **[state lifecycle management functions](_src_adaptableoptions_stateoptions_.stateoptions.html)** .  These State Options offer a superior and more flexible alternative to Config Server and are recommended best practice.  They allow full control over managing state hydration, rehydration and persisttence and are more suitable for all users.
+ * Note that since v.6 AdapTable provides **[state lifecycle management functions](_src_adaptableoptions_stateoptions_.stateoptions.html)** .  These **State Options offer a superior and more flexible alternative to Config Server and are recommended best practice**.  They allow full control over managing state hydration, rehydration and persisttence and are more suitable for all users and will be actively managed going forward.
  *
  * To use ConfigServer you need to set the ​​`enableConfigServer​` property to ​true​ and also provide a value for the ​`configServerUrl`​ property (where your config will be stored).
  *
