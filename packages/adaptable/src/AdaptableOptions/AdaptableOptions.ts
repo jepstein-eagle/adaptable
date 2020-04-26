915dbb1bb73b3ebd6da81f374689f9f02c66c8ed
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-04-26 19:24:20 +0300

    documentation for team sharing

--- packages/adaptable/src/AdaptableOptions/AdaptableOptions.ts
+++ packages/adaptable/src/AdaptableOptions/AdaptableOptions.ts
@@ -49,6 +49,7 @@ import { TeamSharingOptions } from './TeamSharingOptions';
  *  | [queryOptions](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#queryoptions)               | Options when running a Query ( [Expression](https://api.adaptabletools.com/modules/_predefinedconfig_common_expression_.html) ) in AdapTable |
  *  | [searchOptions](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#searchoptions)             | Search-related options (e.g. for managing Server searching)   |
  *  | [stateOptions](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#stateoptions)               | Series of functions to allow you to manage AdapTable State    |
+ *  | [teamSharingOptions](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#teamsharingoptions)   | Enables 'Team Sharing' of AdapTable objects among colleagues |
  *  | [userFunctions](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#userfunctions)             | Implementations of User Functions which are referenced in Config |
  *  | [userInterfaceOptions](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#userinterfaceoptions)| User Interface related functions (e.g. menus, toolbars)       |
  *  | [plugins](_src_adaptableoptions_adaptableoptions_.adaptableoptions.html#plugins)                         | Options used by the AdapTable plugins (e.g. charting, finance)|
@@ -254,7 +255,15 @@ export interface AdaptableOptions {
   userInterfaceOptions?: UserInterfaceOptions;
 
   /**
-   * Options for managing 'Team Sharing' - allows users to create Adaptable Objects and share with colleagues
+   * Options for managing 'Team Sharing'
+   *
+   * This allows users to create Adaptable Objects and share with colleagues
+   *
+   * Includes 2 functions:
+   *
+   * - *getSharedEntities*: retrieves any available Shared Entities for the user to download
+   *
+   * - *setSharedEntities*: saves (essentially uploads) Shared Entities so they can be re-used by other members of the team
    *
    */
   teamSharingOptions?: TeamSharingOptions;
--- packages/adaptable/src/AdaptableOptions/TeamSharingOptions.ts
+++ packages/adaptable/src/AdaptableOptions/TeamSharingOptions.ts
@@ -1,7 +1,49 @@
 import { SharedEntity } from '../PredefinedConfig/TeamSharingState';
 
+/**
+ * Options for managing **Team Sharing** where Adaptable Objects can be shared between colleagues.
+ *
+ * Includes 2 functions
+ *
+ * - *getSharedEntities*: retrieves any available Shared Entities for the user to download
+ *
+ * - *setSharedEntities*: saves (essentially uploads) Shared Entities so they can be re-used by other members of the team
+ *
+ * ```ts
+ * teamSharingOptions: {
+ *     enableTeamSharing: true,
+ *      async getSharedEntities(adaptableId) {
+ *        return new Promise(resolve => {
+ *          const sharedEntities = JSON.parse(
+ *            localStorage.getItem(`TEAM_SHARING:${adaptableId}`) || '[]'
+ *          );
+ *          setTimeout(() => resolve(sharedEntities), 1000);
+ *        });
+ *      },
+ *      async setSharedEntities(adaptableId, sharedEntities) {
+ *        return new Promise(resolve => {
+ *          localStorage.setItem(`TEAM_SHARING:${adaptableId}`, JSON.stringify(sharedEntities));
+ *          setTimeout(() => resolve(), 1000);
+ *        });
+ *      },
+ *    },
+ * ```
+ */
 export interface TeamSharingOptions {
+  /**
+   * Whether Team Sharing is enabled
+   *
+   * **Default Value: false**
+   */
   enableTeamSharing: boolean;
+
+  /**
+   * Function which retrieves any available Shared Entities that the user is able to download (and merge automatically with his Adaptable State)
+   */
   getSharedEntities: (adaptableId: string) => Promise<SharedEntity[]>;
+
+  /**
+   * Function which saves (essentially uploads) Shared Entities so they can be downloaded (and re-used) by other members of the team
+   */
   setSharedEntities: (adaptableId: string, sharedEntities: SharedEntity[]) => Promise<void>;
 }
