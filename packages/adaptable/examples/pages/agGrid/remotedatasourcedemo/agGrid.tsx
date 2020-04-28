cc9eac3ddad79292102a6abd4ce03fd3967bb0c4
roblotter <roblotter1@gmail.com>
2020-04-28 13:26:20 +0300

    Add release script for publishing types in public npm package

--- packages/adaptable/examples/pages/agGrid/remotedatasourcedemo/agGrid.tsx
+++ packages/adaptable/examples/pages/agGrid/remotedatasourcedemo/agGrid.tsx
@@ -8,9 +8,14 @@ import '../../../../src/index.scss';
 
 import '../../../../src/themes/dark.scss';
 
-import { GridOptions } from '@ag-grid-community/all-modules';
+import { GridOptions, ColDef } from '@ag-grid-community/all-modules';
 import Adaptable from '../../../../src/agGrid';
-import { AdaptableOptions, PredefinedConfig } from '../../../../src/types';
+import {
+  AdaptableOptions,
+  PredefinedConfig,
+  SearchChangedEventArgs,
+  AdaptableSearchState,
+} from '../../../../src/types';
 import { AllEnterpriseModules } from '@ag-grid-enterprise/all-modules';
 
 function InitAdaptableDemo() {
@@ -24,10 +29,15 @@ function InitAdaptableDemo() {
       { field: 'CompanyName', type: 'abColDefString' },
       { field: 'ContactName', type: 'abColDefString' },
       { field: 'ItemCount', type: 'abColDefNumber' },
-    ],
+      { field: 'ShipVia', type: 'abColDefString' },
+    ].map((c: ColDef) => {
+      c.filter = true;
+      return c;
+    }),
     rowData: undefined,
     enableRangeSelection: true,
     floatingFilter: true,
+
     suppressColumnVirtualisation: true,
     suppressMenuHide: true,
     rowHeight: 30,
@@ -74,11 +84,43 @@ function InitAdaptableDemo() {
   };
   adaptableOptions.predefinedConfig = demoConfig;
   const adaptableApi = Adaptable.init(adaptableOptions);
-  fetch('https://json-server.adaptabletools.com/orders?_limit=1000')
-    .then(response => response.json())
-    .then(data => {
-      adaptableApi.gridApi.setGridData(data);
+
+  adaptableApi.eventApi.on('SearchChanged', (args: SearchChangedEventArgs) => {
+    const { data } = args;
+    const searchState = data[0].id.adaptableSearchState;
+    console.log(searchState);
+
+    reload({
+      quickSearch: searchState.quickSearch,
+      columnFilters: searchState.columnFilters,
     });
+  });
+
+  const reload = (searchState: Partial<AdaptableSearchState>) => {
+    const arg: any = {
+      quickSearch: searchState.quickSearch,
+    };
+    if (searchState.columnFilters) {
+      arg.columnFilters = JSON.stringify(searchState.columnFilters);
+    }
+
+    const toQuery = (arg: any): string =>
+      Object.keys(arg)
+        .map(key => {
+          const value = arg[key];
+          return value != null ? `at_${key}=${value}` : null;
+        })
+        .filter(Boolean)
+        .join('&');
+
+    fetch(`http://localhost:3001/api/orders?_limit=10&` + toQuery(arg))
+      .then(response => response.json())
+      .then(data => {
+        adaptableApi.gridApi.setGridData(data);
+      });
+  };
+
+  reload({});
 }
 
 let demoConfig: PredefinedConfig = {};
