b49aa714b79796c20f6a92dbed84e75611fba09d
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-04-28 08:50:54 +0300

    team sharing documentation

--- packages/adaptable/src/PredefinedConfig/TeamSharingState.ts
+++ packages/adaptable/src/PredefinedConfig/TeamSharingState.ts
@@ -3,13 +3,15 @@ import { AdaptableObject } from './Common/AdaptableObject';
 import { AdaptableFunctionName } from '../types';
 
 /**
- * The Predefined Configuration for the Team Sharing function
+ * The Predefined Configuration for the Team Sharing AdapTable Function
  *
  * Team sharing allows users to share part of their own configuration data with other users (e.g. if one user wants to share a "Pricing View" Layout which has been created with another user).
  *
  * As soon as new Adaptable Objects are created, they can be instantly shared with team members.
  *
  * To share an item click the share button that appears at the end of the row and this will put the Adaptable Object into a storage location where it can then be downloaded by other team members via the Team Sharing popup.
+ *
+ * **Note**: This section is **Internal State** and is not saved into the User's state
  */
 export interface TeamSharingState extends InternalState {
   SharedEntities: SharedEntity[];
