6b6cf57477189a63e8b1a6c09a0240694ab960c2
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-05-15 13:01:09 +0300

    updates

--- packages/adaptable/examples/pages/ExamplesHelper.ts
+++ packages/adaptable/examples/pages/ExamplesHelper.ts
@@ -634,6 +634,25 @@ export class ExamplesHelper {
   }
 
   protected getCountries(): string[] {
+    const newcountries = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      //    '8',
+      //     '9',
+      //     '10',
+      //    '11',
+      //    '12',
+      //    '13',
+      //    '14',
+      //    '15',
+    ];
+    return newcountries;
+
     const countries = [
       //  'Argentina',
       'Australia',
