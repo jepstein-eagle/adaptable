7f2f56dec7db762f054e40a45f5c93f34d25cd65
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-05-18 10:54:20 +0300

    testing flashing with cell class rules (seems ok...)

--- packages/adaptable/examples/pages/agGrid/flashingcellsdemo/agGrid.tsx
+++ packages/adaptable/examples/pages/agGrid/flashingcellsdemo/agGrid.tsx
@@ -7,7 +7,7 @@ import '../../../../src/index.scss';
 import '../../../../src/themes/dark.scss';
 import './index.css';
 
-import { GridOptions, RowNode } from '@ag-grid-community/all-modules';
+import { GridOptions, RowNode, ColDef } from '@ag-grid-community/all-modules';
 import {
   AdaptableOptions,
   PredefinedConfig,
@@ -28,11 +28,46 @@ var api: AdaptableApi;
 
 function InitAdaptableDemo() {
   const examplesHelper = new ExamplesHelper();
-  const tradeCount: number = 100;
+  const tradeCount: number = 30;
   const tickingDataHelper = new TickingDataHelper();
   const tradeData: any = examplesHelper.getTrades(tradeCount);
   const gridOptions: GridOptions = examplesHelper.getGridOptionsTrade(tradeData);
-  //gridOptions.sideBar = 'false';
+
+  let coldefs: ColDef[] = gridOptions.columnDefs as ColDef[];
+
+  // lets try the different ways of colouring cells
+  // see:  https://www.ag-grid.com/javascript-grid-cell-styles/  for more info
+
+  // 1.  Using simple cellStyle - for the ASK column
+  let askColDef: ColDef | undefined = coldefs.find(c => c.field == 'ask');
+  if (askColDef) {
+    askColDef.cellStyle = function(params) {
+      if (params.value < 120) {
+        return { color: 'white', backgroundColor: 'pink' };
+      } else {
+        return null;
+      }
+    };
+  }
+
+  // 2.  Using cellClass - for the Notional column
+  let notionalColDef: ColDef | undefined = coldefs.find(c => c.field == 'notional');
+  if (notionalColDef) {
+    notionalColDef.cellClass = function(params) {
+      return params.value > 1200 ? 'demo-purple' : 'demo-grey';
+    };
+  }
+
+  // 3. Using CellClassRules - for the BID column
+  let bidColDef: ColDef | undefined = coldefs.find(c => c.field == 'bid');
+  if (bidColDef) {
+    bidColDef.cellClassRules = {
+      'demo-brown': function(params: any) {
+        return params.value < 120;
+      },
+    };
+  }
+
   const adaptableOptions: AdaptableOptions = {
     primaryKey: 'tradeId',
     userName: 'Demo User',
@@ -60,8 +95,21 @@ let demoConfig: PredefinedConfig = {
   Dashboard: {
     VisibleButtons: ['FlashingCells'],
   },
+  Layout: {
+    CurrentLayout: 'Flashing',
+    Layouts: [
+      {
+        Name: 'Flashing',
+        Columns: ['tradeId', 'ask', 'notional', 'bid', 'price'],
+      },
+    ],
+  },
   FlashingCell: {
     FlashingCells: [
+      {
+        IsLive: true,
+        ColumnId: 'notional',
+      },
       {
         IsLive: true,
         ColumnId: 'ask',
