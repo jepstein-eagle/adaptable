000b107f7f34fca878cae9940875884b5af1d386
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-07-07 15:58:41 +0300

    quick search docs

--- packages/adaptable/src/PredefinedConfig/QuickSearchState.ts
+++ packages/adaptable/src/PredefinedConfig/QuickSearchState.ts
@@ -1,34 +1,64 @@
 import { AdaptableStyle } from './Common/AdaptableStyle';
 import { ConfigState } from './ConfigState';
 
+/**
+ * The Predefined Configuration for the Quick Search function
+ *
+ * Quick Search runs a simply text-based search across all **visible columns**
+ *
+ * There are wildcards available (e.g s* for >15) to improve the search behaviour.
+ *
+ * --------------
+ *
+ *  ### Further AdapTable Help Resources
+ *
+ * - [Quick Search Demo](https://demo.adaptabletools.com/search/aggridquicksearchdemo/)
+ *
+ * - {@link QuickSearchApi|Quick Search API}
+ *
+ * - {@link SearchOptions|Search Options}
+ *
+ * - [Quick Search Read Me](https://github.com/AdaptableTools/adaptable/blob/master/packages/adaptable/readme/functions/quick-search-function.md)
+ *
+ *
+ * --------------
+ *
+ * ### Quick Search Predefined Config Example
+ *
+ * ```ts
+ * export default {
+ * QuickSearch: {
+ *    QuickSearchText:"j*",
+ *    DisplayAction:"ShowRowAndHighlightCell",
+ *    Style: {
+ *      BackColor: '#ffff00',
+ *      ForeColor: '#8b0000',
+ *   },
+ *  }
+ * } as PredefinedConfig;
+ * ```
+ * In this example we have set up Quick Search to run a search at startup for all cells whose contents start with 'j'.
+ *
+ * Only rows which have matching cells will be displayed and highlighted with the back adn fore colors specified in the Style.
+ *
+ */
 export interface QuickSearchState extends ConfigState {
+  /**
+   * What Quick Search will run at start up.
+   *
+   * Its very rare to set this property in config
+   */
   QuickSearchText?: string;
+
+  /**
+   * How AdapTable will show the Quick Search results
+   *
+   * Can either highight matching cells, filter matching rows or both
+   */
   DisplayAction?: 'HighlightCell' | 'ShowRow' | 'ShowRowAndHighlightCell';
+
+  /**
+   * The style to use for Quick Search - uses the standard `AdaptableStyle` Object.
+   */
   Style?: AdaptableStyle;
 }
-
-/*
-QuickSearchText
-
-string
-
-What Quick Search will look up. Its very rare to set this property in config.
-
-DisplayAction
-
-string
-
-How Adaptable should show the results. There are 3 options for this property:
-
-HighlightCell - The grid shows all rows and cells that match the quick search are highlighted
-
-ShowRow - Only rows that contain cells which match the quick search are shown (but not highlighted)
-
-ShowRowAndHighlightCell - Only rows that contain cells which match the quick search are shown, with matching cells highlighted
-
-Style
-
-Style object
-
-The style to use for Quick Search - see Style Object Config for more details.
-*/
