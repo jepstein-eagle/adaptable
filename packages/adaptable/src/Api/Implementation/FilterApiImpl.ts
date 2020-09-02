159f871cc42f9662443c2cc6352d3fe64e51cc6a
Andrei Canta <andrei.canta@deiu.ro>
2020-09-02 14:53:49 +0300

    skip filters with empty inputs

--- packages/adaptable/src/Api/Implementation/FilterApiImpl.ts
+++ packages/adaptable/src/Api/Implementation/FilterApiImpl.ts
@@ -173,9 +173,9 @@ export class FilterApiImpl extends ApiBase implements FilterApi {
       return true;
     }
 
-    // if (value === null || value === undefined) {
-    //   return false;
-    // }
+    if (columnFilter.Predicate.Inputs?.some(input => StringExtensions.IsNullOrEmpty(input))) {
+      return true;
+    }
 
     const displayValue = this.adaptable.getValueFromRowNode(
       node,
